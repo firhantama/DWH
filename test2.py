ce1755c48b8196b605cc429026eab69f7c39be59
roasteRKopsus <65092706+roasteRKopsus@users.noreply.github.com>
2022-04-05 15:35:13 +0700

    qq

--- test2.py
+++ test2.py
@@ -9,10 +9,11 @@ import pandas as pd
 from pandas.io import sql
 from sqlalchemy import create_engine
 
-df = pd.read_csv('test_dwh.csv', usercols=['STORE', 'ID_BILL', 'CREATE_BY', 'DATE_CREATE', 'STATUS', 'CANCEL_NOT', 'SEQ', 'EI_TA', 'ITEM_QTY', 'STATUS_item', 'DISCOUNT', 'CANCEL', 'ROW_NUMBER'], skiprows=skip)
+df = pd.read_csv('test_dwh.csv', usecols=['STORE', 'ID_BILL', 'CREATE_BY', 'DATE_CREATE', 'STATUS', 'CANCEL_NOTE', 'SEQ', 'EI_TA', 'ITEM_QTY', 'STATUS_item', 'DISCOUNT', 'CANCEL', 'ROW_NUMBER'],)
 print(df)
 
 engine = create_engine('mysql://root:@127.0.0.1/data_pos')
+print (engine)
 with engine.connect() as conn, conn.begin():
     df.to_sql('data_pos.pos', conn, if_exists='replace')
 
