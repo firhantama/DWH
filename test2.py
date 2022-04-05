6769855738bf5a83d3ec5999e1bf166e8e16a0d9
roasteRKopsus <65092706+roasteRKopsus@users.noreply.github.com>
2022-04-05 15:59:17 +0700

    ade2e32

--- test2.py
+++ test2.py
@@ -12,8 +12,9 @@ from sqlalchemy import create_engine
 df = pd.read_csv('test_dwh.csv', usecols=['STORE', 'ID_BILL', 'CREATE_BY', 'DATE_CREATE', 'STATUS', 'CANCEL_NOTE', 'SEQ', 'EI_TA', 'ITEM_QTY', 'STATUS_item', 'DISCOUNT', 'CANCEL', 'ROW_NUMBER'],)
 print(df)
 
-engine = create_engine('mysql://root:@127.0.0.1/data_pos')
+engine = create_engine('mysql://root:@127.0.0.1/data_pos').raw_connection()
 engine.connect()
+cursor = engine.cursor()
 print (engine.connect())
 with engine.connect() as conn, conn.begin():
     df.to_sql('data_pos.pos', conn, if_exists='replace')
