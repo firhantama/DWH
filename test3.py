133a16f919c45060a956326ff679f52bc1656ebe
roasteRKopsus <65092706+roasteRKopsus@users.noreply.github.com>
2022-04-11 16:17:17 +0700

    gsdoa

--- test3.py
+++ test3.py
@@ -9,8 +9,11 @@ cursor = mydb.cursor()
 csv_data = csv.reader('test_dwh.csv')
 for row in csv_data:
 
+    # cursor.execute('INSERT INTO data_pos.pos(STORE, ID_BILL, CREATE_BY, DATE_CREATE, STATUS, CANCEL_NOT, SEQ, EI_TA, ITEM_QTY, STATUS_item, DISCOUNT, CANCEL, ROW_NUMBER)' \
+    #       'VALUES("%s", "%s", "%s","%s","%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")', 
+    #       row)
     cursor.execute('INSERT INTO data_pos.pos(STORE, ID_BILL, CREATE_BY, DATE_CREATE, STATUS, CANCEL_NOT, SEQ, EI_TA, ITEM_QTY, STATUS_item, DISCOUNT, CANCEL, ROW_NUMBER)' \
-          'VALUES("%s", "%s", "%s","%s","%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")', 
+          'VALUES({0}, {1}, {2},{3},{4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}, {12})', 
           row)
 #close the connection to the database.
 mydb.commit()
