63d94a5bfc4dd80b562d88b857fb175d705a67c4
roasteRKopsus <65092706+roasteRKopsus@users.noreply.github.com>
2022-04-14 01:49:11 +0700

    tinggal benerin tanggal

--- test3.py
+++ test3.py
@@ -21,13 +21,14 @@ with open('test_dwh.csv', 'r') as file:
         #       'VALUES(%s, %s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', 
         #       row)
 
-        cursor.execute('INSERT INTO tuku_pos.data_sales(ROW_NUMBER, Payment_method, Jenis_customer, ID_member, ID_STORE, STORE, ID_BILL, CREATE_BY, TRANS_DATE, DATE_CREATE, BILL_STATUS, STATUS, USER_CANCEL, CANCEL_NOTE, SEQ, STATUS_item, ID_ITEM, NAME, NOTE, ITEM_QTY, UNIT_PRICE, DISCOUNT_AMOUNT, TOTAL_AMOUNT)' \
-              'VALUES(%s, %s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', 
-              row)
+        cursor.execute('INSERT INTO data_sales (NO_ROW, Payment_method, Jenis_customer, ID_member, ID_STORE, STORE, ID_BILL, CREATE_BY, TRANS_DATE, DATE_CREATE, BILL_STATUS, STATUS, USER_CANCEL, CANCEL_NOTE, SEQ, STATUS_item, ID_ITEM, NAME, NOTE, ITEM_QTY, UNIT_PRICE, DISCOUNT_AMOUNT, TOTAL_AMOUNT) VALUES ("%s", "%s", "%s" ,"%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s");', row)
+        # cursor.execute("INSERT INTO tuku_pos.data_sales" 
+        #       'VALUES("%s", "%s", "%s" ,"%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s");', 
+        #       row)
         # cursor.execute('INSERT INTO dwh_project.dwh_project(STORE, ID_BILL, CREATE_BY, DATE_CREATE, STATUS, CANCEL_NOT, SEQ, EI_TA, ITEM_QTY, STATUS_item, DISCOUNT, CANCEL, ROW_NUMBER)' \
         #     'VALUES({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})', 
         #     row)
-        mydb.commit()
+    mydb.commit()
    
 #close the connection to the database.
 
