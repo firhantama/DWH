d0ffd2988a2b11c321bc4bc6cb4ffebc7d3d7d8d
roasteRKopsus <65092706+roasteRKopsus@users.noreply.github.com>
2022-04-14 16:42:43 +0700

    kk

--- test5azka.py
+++ test5azka.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from sqlalchemy import create_engine, types
 
+def import_to_mysql(filename, table_name, db_user, db_pass, address, db_name):
+    connection_address = 'mysql://{}:{}@{}/{}'.format(db_user, db_pass, address, db_name)
+    print(connection_address)
+    engine = create_engine(connection_address) # enter your password and database names here
+    print(engine)
+    df = pd.read_csv(filename,sep=',',encoding='utf8')
+    print(df) # Replace Excel_file_name with your excel sheet name
+    df.to_sql(table_name, con=engine, index=False, if_exists='append', chunksize=1000,)
+    print('done to server')
 
-engine = create_engine('mysql://admin:y$UYRPB&7Go5@database-1.cfdamesx7fal.ap-southeast-1.rds.amazonaws.com:3306/tuku_pos') # enter your password and database names here
-# engine.execute('select * from data_pos')
-print(engine)
-df = pd.read_csv("test_dwh.csv",sep=',',encoding='utf8') # Replace Excel_file_name with your excel sheet name
-# show_all = engine.execute('select * from data_pos')
-df.to_sql('data_sales',con=engine,index=False,if_exists='append', chunksize=1000,)
-# for x in show_all:
-#     print(x)
-# print (df)
+import_to_mysql(filename= 'test_dwh.csv', table_name='data_sales', db_user='admin', db_pass='y$UYRPB&7Go5', 
+address='database-1.cfdamesx7fal.ap-southeast-1.rds.amazonaws.com:3306', db_name='tuku_pos' )
